ca6494df46d28fe6198d218564edc7653dfd8e43
LuisaFC <luisabsb@gmail.com>
2024-09-06 13:27:37 -0300

    feat: router

---
+++ api/src/router.js
@@ -0,0 +1,48 @@
+import { Router } from 'express'
+
+export const router = Router();
+
+//List Categories
+router.get('/categories', (req, res) => {
+  res.send('Ok')
+})
+
+// Create Catergories
+router.post('/categories', (req, res) => {
+  res.send('Ok')
+})
+
+//List Products
+router.get('/products', (req, res) => {
+  res.send('Ok')
+})
+
+// Create Products
+router.post('/products', (req, res) => {
+  res.send('Ok')
+})
+
+// Get product by category
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('Ok')
+})
+
+//List orders
+router.get('/orders', (req, res) => {
+  res.send('Ok')
+})
+
+//Create order
+router.post('/orders', (req, res) => {
+  res.send('Ok')
+})
+
+// Change order status
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('Ok')
+})
+
+//Delete order
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('Ok')
+})
